456473f591d4408c0629e7e352bdb8b492ddf061
LaOwlLol <something.less.than.nate@gmail.com>
2018-10-01 22:49:33 -0500

    demo MiniMapWorldView, need to extract it in to it's own component.

--- src/main/java/fauxpas/apps/WorldTiler.java
+++ src/main/java/fauxpas/apps/WorldTiler.java
@@ -4,6 +4,7 @@ import fauxpas.collections.TileImageDirectory;
 import fauxpas.components.TileEditor;
 import fauxpas.entities.Tile;
 import fauxpas.entities.World;
+import fauxpas.views.MiniMapWorldView;
 import fauxpas.views.ScrollableWorldView;
 import javafx.animation.AnimationTimer;
 import javafx.application.Application;
@@ -12,20 +13,22 @@ import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.layout.AnchorPane;
 import javafx.scene.Scene;
+import javafx.scene.paint.Color;
 import javafx.stage.Stage;
 import java.nio.file.Paths;
 
 
 public class WorldTiler extends Application {
 
-    private static final int VIEW_WIDTH_TILES = 4;
-    private static final int VIEW_HEIGHT_TILES = 3;
+    private static final int VIEW_WIDTH_TILES = 8;
+    private static final int VIEW_HEIGHT_TILES = 5;
 
     private TileEditor editor;
+    private MiniMapWorldView miniMap;
 
     public WorldTiler() {
 
-        int tile_Dim = 150;
+        int tile_Dim = 75;
 
         //define some tiles.
         Tile blank = new Tile("water",
@@ -48,10 +51,16 @@ public class WorldTiler extends Application {
         );*/
 
         //construct a world with blank tile.
-        World world = new World(10,10, blank);
+        World world = new World(30,30, blank);
 
         //set a tile to grass.
         world.setTile(1,0, grass);
+
+        for (int j = 15; j < 25; ++j) {
+            for (int i = 15; i < 25; ++i) {
+                world.setTile(i, j, grass);
+            }
+        }
 
         //construct a view on the world using the asset set.
         ScrollableWorldView view = new ScrollableWorldView(0,0, VIEW_WIDTH_TILES, VIEW_HEIGHT_TILES, world, assets);
@@ -60,6 +69,8 @@ public class WorldTiler extends Application {
         Canvas canvas = new Canvas(tile_Dim*VIEW_WIDTH_TILES,tile_Dim*VIEW_HEIGHT_TILES);
 
         this.editor = new TileEditor(world, assets, view, canvas);
+
+        this.miniMap = new MiniMapWorldView(0, 0, 200, 125, world, assets);
     }
 
     @Override
@@ -72,6 +83,22 @@ public class WorldTiler extends Application {
 
         this.editor.startRender();
         this.editor.initEventHandlers();
+
+        Canvas miniCanvas = new Canvas(200, 125);
+
+        root.getChildren().add(miniCanvas);
+        root.setLeftAnchor(miniCanvas, 10.0);
+        root.setBottomAnchor(miniCanvas, 10.0);
+
+        AnimationTimer animTimer = new AnimationTimer() {
+            @Override
+            public void handle(long now) {
+                miniCanvas.getGraphicsContext2D().clearRect(0,0,200, 125);
+                miniMap.render(miniCanvas.getGraphicsContext2D());
+            }
+        };
+
+        animTimer.start();
 
         Scene scene = new Scene(root, 1024, 768);
         primaryStage.setScene(scene);
