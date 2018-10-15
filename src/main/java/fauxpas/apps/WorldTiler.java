7b3fdb2869f24fcc08c2187951cd2d2d66e229d8
LaOwlLol <something.less.than.nate@gmail.com>
2018-10-14 21:14:39 -0500

    more clean up, creating setup dialogues and trying to remove as much hard coded values (paths to assets and dimensions).

--- src/main/java/fauxpas/apps/WorldTiler.java
+++ src/main/java/fauxpas/apps/WorldTiler.java
@@ -22,13 +22,12 @@ import javafx.stage.FileChooser;
 import javafx.stage.Stage;
 
 import java.io.File;
+import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Optional;
 import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.stream.Collectors;
 import java.util.stream.IntStream;
-import java.util.stream.Stream;
 
 
 public class WorldTiler extends Application {
@@ -42,7 +41,8 @@ public class WorldTiler extends Application {
     private EditorMiniMap miniMap;
     private AssetPalette palette;
 
-    private Tile blank;
+    private Tile primaryTile;
+    private Tile secondaryTile;
     private TileImageDirectory assets;
     private int tile_dim;
     private ScrollableWorldView scrollableView;
@@ -57,38 +57,64 @@ public class WorldTiler extends Application {
 
     @Override
     public void start(Stage primaryStage) {
+        InitWorldTiler(primaryStage);
+    }
+
+    private void InitWorldTiler(Stage primaryStage) {
+
         primaryStage.setTitle("World-Tiler");
         AnchorPane root = new AnchorPane();
 
-        Optional<String> result = CollectInitialParameters(primaryStage);
+        Optional<String> dirResult = CollectInitialParameters(primaryStage);
 
-        result.ifPresent( (data) -> {
+        dirResult.ifPresent( (worldData) -> {
 
-            setupAssets( Integer.parseInt(data.split(":")[2]), data.split(":")[0], data.split(":")[1] );
-            constructWorld();
+            if (Files.exists(Paths.get(worldData.split(":")[0], "water", "water_0.png")) &&
+                  Files.exists(Paths.get(worldData.split(":")[0], "grass", "grass_0.png"))) {
 
-            setupEditor(root);
-            setupMiniMap(root);
-            setupPalette(root);
-
-            registerMiniMapViewToScrollViewUpdates();
-            registerScrollViewToPaletteUpdates();
-
-            Scene scene = new Scene(root, 1024, 768);
-            primaryStage.setScene(scene);
-            primaryStage.show();
-        } );
+                setupAssets(Integer.parseInt(worldData.split(":")[1]),
+                      worldData.split(":")[0],
+                      Paths.get(worldData.split(":")[0], "water", "water_0.png").toString(),
+                      Paths.get(worldData.split(":")[0], "grass", "grass_0.png").toString());
 
 
+            }
+            else {
+                Optional<String> tileResults = CollectTileDefaults(primaryStage);
+
+                tileResults.ifPresent( tileData -> {
+                    setupAssets(Integer.parseInt(worldData.split(":")[1]),
+                          worldData.split(":")[0],
+                          tileData.split(":")[0],
+                          tileData.split(":")[1]);
+                });
+            }
+
+            if (this.assets != null && this.primaryTile != null && this.secondaryTile != null) {
+
+                constructWorld( worldData.split(":")[2] );
+
+                setupEditor(root);
+                setupMiniMap(root);
+                setupPalette(root);
+
+                registerMiniMapViewToScrollViewUpdates();
+                registerScrollViewToPaletteUpdates();
+
+                Scene scene = new Scene(root, 1024, 768);
+                primaryStage.setScene(scene);
+                primaryStage.show();
+            }
+        });
     }
 
     private Optional<String> CollectInitialParameters(Stage primaryStage) {
         Dialog<String> setupInfoCollector = new Dialog<>();
+        setupInfoCollector.setTitle("Setup Assets...");
         GridPane infoCollectGrid = new GridPane();
         infoCollectGrid.setHgap(10);
         infoCollectGrid.setVgap(4);
         infoCollectGrid.setPadding(new Insets(0, 10, 0, 10));
-
 
         AtomicBoolean dirSelected = new AtomicBoolean(false);
         Label assetsLabel = new Label("Assets Directory: ");
@@ -106,37 +132,95 @@ public class WorldTiler extends Application {
         infoCollectGrid.add(directorySelect, 2, 1, 18, 1);
         GridPane.setHalignment(directorySelect, HPos.RIGHT);
 
-        AtomicBoolean blankSelected = new AtomicBoolean(false);
-        Label blankLabel = new Label("Blank tile: ");
-        Button blankSelect = new Button("None");
-        blankSelect.setOnMouseClicked((event) -> {
-            FileChooser assetsSelector = new FileChooser();
-            assetsSelector.setTitle("Choose blank tile");
-            File assetsDir = assetsSelector.showOpenDialog(primaryStage);
-            blankSelect.setText(assetsDir.toPath().toString());
-            blankSelected.set(true);
+        Label worldLabel = new Label("World File: ");
+        Button worldSelect = new Button("None");
+        worldSelect.setOnMouseClicked((event) -> {
+            FileChooser worldSelector = new FileChooser();
+            worldSelector.setTitle("Choose a world file");
+            File assetsDir = worldSelector.showOpenDialog(primaryStage);
+            worldSelect.setText(assetsDir.toPath().toString());
         });
 
-        infoCollectGrid.add(blankLabel, 1, 3);
-        GridPane.setHalignment(blankLabel, HPos.LEFT);
-        infoCollectGrid.add(blankSelect, 2, 3, 18, 1);
-        GridPane.setHalignment(blankSelect, HPos.RIGHT);
+        infoCollectGrid.add(worldLabel, 1, 3);
+        GridPane.setHalignment(worldLabel, HPos.LEFT);
+        infoCollectGrid.add(worldSelect, 2, 3, 18, 1);
+        GridPane.setHalignment(worldSelect, HPos.RIGHT);
 
         AtomicBoolean dimSelected = new AtomicBoolean(false);
         Label assetDims = new Label("Tile dimension: ");
         ArrayList<Integer> options = new ArrayList<>();
         IntStream.range(20, 76).filter(i ->  i%5 == 0 ).forEach(options::add);
         ChoiceBox<Integer> dimOptions = new ChoiceBox(FXCollections.observableArrayList(options));
-        dimOptions.getSelectionModel().selectedItemProperty().addListener( (observable, oldVal, newVal) -> dimSelected.set(true)  );
+        dimOptions.getSelectionModel().selectedItemProperty().addListener(
+              (observable, oldVal, newVal) -> dimSelected.set(true));
 
-        infoCollectGrid.add(assetDims, 1, 5);
-        infoCollectGrid.add(dimOptions, 3, 5 );
+        infoCollectGrid.add(assetDims, 1, 4);
+        infoCollectGrid.add(dimOptions, 3, 4 );
 
         ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
         setupInfoCollector.getDialogPane().getButtonTypes().add(buttonTypeOk);
         setupInfoCollector.setResultConverter( buttonType -> {
-            if (buttonType == buttonTypeOk && dimSelected.get() && blankSelected.get() && dirSelected.get()) {
-                return directorySelect.getText()+":"+blankSelect.getText()+":"+dimOptions.getValue().toString();
+            if (buttonType == buttonTypeOk && dimSelected.get() && dirSelected.get()) {
+                return directorySelect.getText()+":"+
+                      dimOptions.getValue().toString()+":"+
+                      worldSelect.getText();
+
+            }
+            else {
+                return null;
+            }
+        });
+
+        infoCollectGrid.autosize();
+        setupInfoCollector.getDialogPane().setContent(infoCollectGrid);
+        return setupInfoCollector.showAndWait();
+    }
+
+    private Optional<String> CollectTileDefaults(Stage primaryStage) {
+        Dialog<String> setupInfoCollector = new Dialog<>();
+        setupInfoCollector.setTitle("Select default tiles...");
+        GridPane infoCollectGrid = new GridPane();
+        infoCollectGrid.setHgap(10);
+        infoCollectGrid.setVgap(4);
+        infoCollectGrid.setPadding(new Insets(0, 10, 0, 10));
+
+        AtomicBoolean waterSelected = new AtomicBoolean(false);
+        Label waterLabel = new Label("Water tile: ");
+        Button waterSelect = new Button("None");
+        waterSelect.setOnMouseClicked((event) -> {
+            FileChooser assetsSelector = new FileChooser();
+            assetsSelector.setTitle("Choose water tile");
+            File assetsDir = assetsSelector.showOpenDialog(primaryStage);
+            waterSelect.setText(assetsDir.toPath().toString());
+            waterSelected.set(true);
+        });
+
+        infoCollectGrid.add(waterLabel, 1, 1);
+        GridPane.setHalignment(waterLabel, HPos.LEFT);
+        infoCollectGrid.add(waterSelect, 2, 1, 18, 1);
+        GridPane.setHalignment(waterSelect, HPos.RIGHT);
+
+        AtomicBoolean grassSelected = new AtomicBoolean(false);
+        Label grassLabel = new Label("Grass tile: ");
+        Button grassSelect = new Button("None");
+        grassSelect.setOnMouseClicked((event) -> {
+            FileChooser assetsSelector = new FileChooser();
+            assetsSelector.setTitle("Choose grass tile");
+            File asset = assetsSelector.showOpenDialog(primaryStage);
+            grassSelect.setText(asset.toPath().toString());
+            grassSelected.set(true);
+        });
+
+        infoCollectGrid.add(grassLabel, 1, 3);
+        GridPane.setHalignment(grassLabel, HPos.LEFT);
+        infoCollectGrid.add(grassSelect, 2, 3, 18, 1);
+        GridPane.setHalignment(grassSelect, HPos.RIGHT);
+
+        ButtonType buttonTypeOk = new ButtonType("Okay", ButtonBar.ButtonData.OK_DONE);
+        setupInfoCollector.getDialogPane().getButtonTypes().add(buttonTypeOk);
+        setupInfoCollector.setResultConverter( buttonType -> {
+            if (buttonType == buttonTypeOk && waterSelected.get() && grassSelected.get()) {
+                return waterSelect.getText()+":"+grassSelect.getText();
             }
             else {
                 return null;
@@ -154,7 +238,7 @@ public class WorldTiler extends Application {
         grid.setVgap(5);
         grid.setPadding(new Insets(5,5,5,5));
 
-        this.palette = new AssetPalette(grid, this.assets, this.blank);
+        this.palette = new AssetPalette(grid, this.assets, this.primaryTile);
         this.palette.initLayout();
 
         root.getChildren().add(grid);
@@ -162,14 +246,15 @@ public class WorldTiler extends Application {
         root.setTopAnchor(grid, 10.0);
     }
 
-    private void setupAssets(int assetsDim, String assetsPath, String blankTilePath) {
+    private void setupAssets(int assetsDim, String assetsPath, String primaryTilePath, String secondaryTilePath) {
         this.tile_dim = assetsDim;
 
         //construct a new assets directory with dims matching local assets.
         this.assets = TileImageDirectory.LoadFromFileSystem(assetsPath, tile_dim, true);
 
         //define some tiles.
-        this.blank = new Tile("water", blankTilePath);
+        this.primaryTile = new Tile("water", primaryTilePath);
+        this.secondaryTile = new Tile( "grass", secondaryTilePath);
     }
 
     private void setupEditor(AnchorPane root) {
@@ -191,7 +276,6 @@ public class WorldTiler extends Application {
         root.getChildren().add(this.editor.getCanvas());
         root.setLeftAnchor(this.editor.getCanvas(), 10.0);
     }
-
 
     private void setupMiniMap(AnchorPane root) {
         //construct a MiniMapWorldView for an over view of the world under construction.
@@ -223,26 +307,24 @@ public class WorldTiler extends Application {
         this.palette.registerChangeListener(this.editor);
     }
 
+    private void constructWorld(String worldPath) {
+        //construct a by world loading from file or with primaryTile tile.
 
-    private void constructWorld() {
-        //construct a by world loading from file or with blank tile.
         this.world = World.ReadFromFile(
-              Paths.get(System.getProperty("user.home"), "WorldTiler", "world_1").toString()).orElseGet(() -> {
+              Paths.get(worldPath).toString()).orElseGet(() -> {
 
                   System.out.println("Generating new World!");
 
-                  //construct a world with blank tile.
+                  //construct a world with primaryTile tile.
                   //TODO we need a default map to load or a default title to use.
-                  World w = new World(300,300, this.blank);
+                  World w = new World(300,300, this.primaryTile);
 
                   //make and island of grass.
 
                   for (int j = 15; j < 25; ++j) {
                       for (int i = 15; i < 25; ++i) {
                           //TODO we need a default map to load or a default title to use.
-                          w.setTile(i, j,
-                                new Tile("grass", Paths.get(System.getProperty("user.home"),
-                                      "WorldTiler", "grass", "grass_0.png" ).toString()));
+                          w.setTile(i, j, this.secondaryTile);
                       }
                   }
                   return w;
@@ -251,8 +333,8 @@ public class WorldTiler extends Application {
 
     }
 
-    private void saveWorld(String name) {
-        World.WriteToFile(this.world,  Paths.get(System.getProperty("user.home"), "WorldTiler", name).toString() );
+    private void saveWorld(String path) {
+        World.WriteToFile(this.world,  Paths.get(path).toString() );
     }
 
 }
